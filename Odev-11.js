677b368bd09445e24397b03f9366a92763e42a57
gamzegizemozturk <146195952+gamzegizemozturk@users.noreply.github.com>
2024-07-23 17:32:25 +0300

    Update Odev-11.js
    done

--- Odev-11.js
+++ Odev-11.js
@@ -7,3 +7,19 @@
 
 Örnek Dizi: [3, 1, 4, 1, 5, 9, 2, 6, 5]
 */
+//1.
+let names = ['John', 'Paul', 'George', 'Ringo'];
+
+
+names.sort((a, b) => a.length - b.length);
+
+console.log(names);
+
+
+//2.
+let numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+
+
+numbers.sort((a, b) => b - a);
+
+console.log(numbers);
